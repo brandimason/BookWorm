2669d5ecaade0197e73188504a3e44b4f782e711
Brandi L Mason <masonbrandi66@gmail.com>
2023-10-26 18:00:40 -0600

    updated seed file

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -8,36 +8,40 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     # do i need this ^ if i am using what's below: the Base.metadata
-    # Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
     session.query(Book).delete()
     session.query(User).delete()
     session.query(Book_Read).delete()
 
     # with Session(engine) as session:
-        #books
-
+    
+    #books
     harrypotter = Book(title = "Harry Potter and the Sorcerer's Stone", author = "JK Rowling")
     untamed = Book(title = "Untamed", author = "Glennon Doyle")
     fouragreements = Book(title = "The Four Agreements", author = "Don Miguel Ruiz")
     twilight = Book(title = "Twilight", author = "Stephenie Meyer")
-    session.add_all([harrypotter,
+    session.add_all([
+        harrypotter,
         untamed,
         fouragreements,
         twilight
     ])
     session.commit()
+
     #users
     stephen = User(firstname = "Stephen", lastname = "Lambert")
     sherina =User(firstname = "Sherina", lastname = "Buenaseda")
     shamim = User(firstname = "Shamim", lastname = "Sharifi")
     yesenia = User(firstname = "Yesenia", lastname = "Follingstag")
-    session.add_all([stephen,
+    session.add_all([
+        stephen,
         sherina,
         shamim,
         yesenia
     ])
     session.commit()
+
     #create many to many relationships
     b1 = Book_Read(book_id = harrypotter.id, user_id = stephen.id)
     b2 = Book_Read(book_id = untamed.id, user_id = sherina.id)
